ab73c08e8b3a9e7bbfa4dc1472c0abfb7194c7b8
Manasi <manasibusam04@gmail.com>
2026-06-01 15:29:47 +0530

    Github OAuth - workspace updates

--- app/api/github/callback/route.ts
+++ app/api/github/callback/route.ts
@@ -1,41 +1,40 @@
-import { NextRequest, NextResponse } from 'next/server';
-export async function GET(req:NextRequest){
-    const code=req.nextUrl.searchParams.get('code');
+import { NextRequest, NextResponse } from "next/server";
 
-    if(!code){
-        return NextResponse.redirect(new URL('/workspace?error=Code not found',req.url))
+export async function GET(req: NextRequest) {
+    const code = req.nextUrl.searchParams.get('code');
+
+    if (!code) {
+        return NextResponse.redirect(new URL('/workspace?error=missing_code', req.url))
     }
 
-    const res=await fetch('https://github.com/login/oauth/access_token', {
+    const res = await fetch('https://github.com/login/oauth/access_token', {
         method: 'POST',
-        headers: {
-            'Content-Type': 'application/json',
-            'Accept': 'application/json'
-        },
-        //hi
-    
+        headers: { 'content-type': 'application/json', Accept: 'application/json' },
         body: JSON.stringify({
-            client_id: process.env.GITHUB_CLIENT_ID,
-            client_secret: process.env.GITHUB_CLIENT_SECRET,
-            code: code
+            client_id: process.env.GITHUB_CLIENT_ID!,
+            client_secret: process.env.GITHUB_CLIENT_SECRET!,
+            code
         })
     })
-    const data=await res.json();
-    const access_token=data.access_token;
 
-    if(!access_token){
-        return NextResponse.redirect(new URL('/workspace?error=Access token not found',req.url))
+    const data = await res.json();
+    const token = data.access_token;
+
+    if (!token) {
+        return NextResponse.redirect(new URL('/workspace?error=token_exchange-failed', req.url))
+
     }
 
-    const response=NextResponse.redirect(new URL('/workspace?success=Github connected successfully',req.url))
+    const response = NextResponse.redirect(new URL('/workspace', req.url));
 
-    response.cookies.set('github_token',access_token,{
-        httpOnly:true,
-        secure:process.env.NODE_ENV==='production',
-        maxAge:60*60*24*30, // 30 days)
-        path:'/',
-        sameSite:'lax'
+    //store token in htto-only cookie
+    response.cookies.set('gh_token', token, {
+        httpOnly: true,
+        secure: process.env.NODE_ENV === 'production',
+        maxAge: 60 * 60 * 24 * 30, //30 days
+        path: '/',
+        sameSite: 'lax'
     });
+
     return response;
 }
-    
