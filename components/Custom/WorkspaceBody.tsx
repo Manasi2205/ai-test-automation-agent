4464ee4327be282d930e8c4c4b5eb09162204545
Manasi <manasibusam04@gmail.com>
2026-06-01 15:17:33 +0530

    Add GitHub OAuth routes

--- components/Custom/WorkspaceBody.tsx
+++ components/Custom/WorkspaceBody.tsx
@@ -1,33 +1,59 @@
 "use client"
 import { UserDetailContext } from '@/context/UserDetailContext';
-import React, { useContext } from 'react'
+import React, { useContext, useState, useEffect } from 'react'
 import Image from 'next/image';
 import { Button } from '../ui/button';
 import { Card, CardContent } from '../ui/card';
-import EmptyWorkspace  from './EmptyWorkspace';
+import EmptyWorkspace from './EmptyWorkspace';
+import axios from 'axios';
+import { useRouter } from 'next/navigation';
 
 export default function WorkspaceBody() {
-    const{ userDetail } = useContext(UserDetailContext);
+    const { userDetail } = useContext(UserDetailContext);
+    const router = useRouter();
+    const [token, setToken] = useState('');
 
-  return (
-    <div>
+    useEffect(() => {
+        GetGithubUserToken();
+    }, [])
+
+    const GetGithubUserToken = async () => {
+        const result = await axios.get('/api/github/token');
+        console.log(result.data.token);
+        setToken(result.data.token);
+    }
+
+    const OnAddRepo = async () => {
+        router.push('/api/github');
+    }
+
+    return (
+        <div>
             <div className='flex justify-between items-center'>
                 <h2 className='text-4xl font-medium'>Workspace</h2>
-                <h2 className='text-blue-800 bg-blue-100 px-2 rounded-lg'>Remaining Credits: {userDetail?.credits}</h2>
+                <h2 className='text-blue-800 bg-blue-100 px-2 rounded-lg'>
+                    Remaining Credits: {userDetail?.credits}
+                </h2>
             </div>
 
-
-        <Card className={'mt-5 flex justify-between items-center p-4 border rounded-lg'}>
+            <Card className='mt-5 flex justify-between items-center p-4 border rounded-lg'>
                 <div className='flex items-center gap-5'>
-                    <Image src={'/github.png'} alt='github' width={40} height={40} />
-                    <h2 className='text-lg' >Connect Github & Add Repository</h2>
+                    <Image src='/github.png' alt='github' width={40} height={40} />
+                    <h2 className='text-lg'>Connect Github & Add Repository</h2>
+                </div>
+                <div>
+                    {!token
+                        ? <Button onClick={OnAddRepo}>Setup</Button>
+                        : <Button onClick={OnAddRepo}>Add Repo</Button>
+                    }
                 </div>
             </Card>
-        <Card className='mt-10'>
-            <CardContent>
-                <EmptyWorkspace />
-            </CardContent>
-        </Card>
-    </div>
-  )
+
+            <Card className='mt-10'>
+                <CardContent>
+                    <EmptyWorkspace />
+                </CardContent>
+            </Card>
+        </div>
+    )
 }
